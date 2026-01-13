bcb02f92dc1d6e2ba783b2da75ea66de50800564
LichtPUBLIC <daffaalyandra65@gmail.com>
2026-01-13 23:21:10 +0700

    Add debug logger for startup crash

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 // Vercel Serverless Function Handler
-const app = require('../server/server');
-
-module.exports = app;
+try {
+    const app = require('../server/server');
+    module.exports = app;
+} catch (error) {
+    console.error('Server Startup Error:', error);
+    // Return an error handler function for Vercel
+    module.exports = (req, res) => {
+        res.status(500).json({
+            message: 'CRITICAL SERVER ERROR (Startup)',
+            error: error.message,
+            stack: error.stack
+        });
+    };
+}
